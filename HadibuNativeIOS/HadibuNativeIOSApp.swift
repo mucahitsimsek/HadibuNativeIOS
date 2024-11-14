6cbaf98c4815b748d7df2a447d42774e6b2f69cb
Mücahit Şimşek <mucahitsmsk74@gmail.com>
2024-11-14 10:43:02 +0300

    Initial Commit

---
+++ HadibuNativeIOS/HadibuNativeIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HadibuNativeIOSApp.swift
+//  HadibuNativeIOS
+//
+//  Created by Mücahit Şimşek on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HadibuNativeIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
